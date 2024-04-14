fec31c9e72af12d0955c1c88ae616b20b3059266
elmoncu <164093323+elmoncu@users.noreply.github.com>
2024-04-14 04:58:35 +0000

    function onclick my function

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-var castle = prompt("Please enter the name of an old building");
-var pool = prompt("Please enter a place where you can take a bath outside");
-var adjective = prompt("Enter an adjective");
-var family = prompt(" Enter the name of a group that life together in the same house");
-var verb = prompt("Enter a verb");
-var theend = prompt("Enter a very common way to say the story is done");
-var myage = prompt("Enter your age");
-function myage2() { if (isNaN(myage) || myage < 0 || myage > 100) alert("You are a lucky man, you do not have to clean up bathrooms in a huge castle"); }
-myage2();
-
-var story = " Once upon in a time I was in an  dream. My dream started in a huge " + castle + ". The castle had a big " + pool + ". It has a lot of rooms. I felt like a " + adjective + " man, and my " + family + " was very happy. The problem started when we had to " + verb + " all rooms. I didn't want to clean these huge rooms and bathrooms!!!. The " + theend + " of this story: when you say how old you are.Tell me your age and I will tell you something you don't know?" + myage + ".";
-document.write(story);
+function myFunction() { 
+document.getElementById("story").innerHTML = story;
 console.log("story: " + story);
+}
+var castle = document.getElementById("noun1").value;
+var pool = document.getElementById("noun2").value;
+var rich = document.getElementById("adjective").value;
+var family = document.getElementById("noun3").value;
+var clean = document.getElementById("verb").value;
+var theend = document.getElementById("noun4").value;
+var myage = document.getElementById("noun4").value;
+function myage2() { if (isNaN(myage) || myage < 0 || myage > 100) alert("You are a lucky man, you do not have to clean up bathrooms in a huge castle");}
+var story = "Once upon in a time I was in a dream. My dream started in a huge "+castle+".The "+castle+"had a big "+pool+", and a lot of rooms. I felt like a "+rich+" man, and my "+family+" was very happy. The problem started when we had to "+clean+"all rooms. None in my "+family+" didn't want "+clean+" these huge rooms and bathrooms!!!. The "+theend+" of this story is when you type how old you are.Tell me your age and I will tell you something funny .";
+// Line 12 & 13 are to save the completed story element as a string data in the database 
+// From line 16 to 26 we are going to save the story in the database.
+var storyData = {
+  timestamp: Date.now(),
+  story: story,
+  castle: castle,
+  pool: pool,
+  adjective: adjective,
+  family: family,
+  verb: verb,
+  theend: theend,
+  myage: myage,
+}
+//From 28 we save data in JSON format ( easy to share and print to console)
+var storyJSON = JSON.stringify(storyData);
+console.log("storyJSON: " + storyJSON);
+//return storyJSON;
+
+
