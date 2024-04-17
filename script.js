d470560cce6b08901f5a74a305251e0f18c252bf
elmoncu <164093323+elmoncu@users.noreply.github.com>
2024-04-17 03:48:47 +0000

    script.js changed

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-function getNoun5() { if (isNaN(myage) || myage < 0 || myage> 100) alert("You MUST typa a number between 0 and 100")
-  else alert("You are a lucky man, MUST to clean up bathrooms in a huge cast") 
-}
+function getNoun5() { if (isNaN(myage) || myage < 0 || myage> 100) alert("You MUST typa a number between 0 and 100");
+}  
 function getStory() { 
 var castle=document.getElementById("noun1").value;
 var pool=document.getElementById("noun2").value;
@@ -9,7 +8,7 @@ var family=document.getElementById("noun3").value;
 var clean=document.getElementById("verb").value;
 var theend=document.getElementById("noun4").value;
 var myage=document.getElementById("noun5").value;
-var story = "Once upon in a time I was in a dream. My dream started in a huge "+castle+".The "+castle+"had a big "+pool+", and a lot of rooms. I felt like a "+rich+" man, and my "+family+" was very happy. The problem started when we had to "+clean+"all rooms. None in my "+family+" didn't want "+clean+" these huge rooms and bathrooms!!!. The "+theend+" of this story is when you type how old you are.Tell me your age and I will tell you something funny .";
+var story = "Once upon in a time I was in a dream. My dream started in a huge "+castle+".The "+castle+"had a big "+pool+", and a lot of rooms. I felt like a "+rich+" man, and my "+family+" was very happy. The problem started when we had to "+clean+"all rooms. None in my "+family+" didn't want "+clean+" these huge rooms and bathrooms!!!. The "+theend+" of this story is when you type how old you are.Tell me your age ";
 document.getElementById("getStory").innerHTML = story;
 console.log("story" + story);
 // Line 11 & 12 are saving the completed story element as a string data in the database 
