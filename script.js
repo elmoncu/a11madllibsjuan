a451820bf2be3eef1b9167a7c75cd15083d329c0
elmoncu <164093323+elmoncu@users.noreply.github.com>
2024-05-02 04:13:50 +0000

    line 75 storyData instead story name

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ console.log("firebase setup complete!");
 
 
 function getNoun5() {
+  var myage = document.getElementById("noun5").value;
   if (isNaN(myage) || myage < 0 || myage > 100) alert("You MUST typa a number between 0 and 100");
 }
 function getStory() {
@@ -27,7 +28,7 @@ function getStory() {
   var storyname = document.getElementById("noun6").value;
   var story = "Once upon in a time I was in a dream. My dream started in a huge " + castle + ".The " + castle + "had a big " + pool + ", and a lot of rooms. I felt like a " + rich + " man, and my " + family + " was very happy. The problem started when we had to " + clean + "all rooms. None in my " + family + " didn't want " + clean + " these huge rooms and bathrooms!!!. The " + theend + " of this story is when you type how old you are.Tell me your age. The name of the story is " + storyname + ".";
   document.getElementById("output").innerHTML = story;
-  console.log("story" + story);
+  console.log("output" + story);
   // Line 11 & 12 are saving the completed story element as a string data in the database 
   // From line ... we are going to save the story in the database.
   var storyData = {
@@ -39,40 +40,56 @@ function getStory() {
     verb: clean,
     noun4: theend,
     noun5: myage,
-    noun6: storyname,
+    storyName: storyname,
     story: story,
   }
   return storyData;
 }
 
-function createMadLib() {
-  console.log("story");
-}
+//function createMadLibstory() {
+  // create a story button
+  //var storyData = getStory();
+  //console.log("storydata: " + storyData)
+//}
+
 function saveMadLib() {
-  //this method saves the madilib to the database
-  console.log("savemadlib() called");
   //first, get the storyData object from crateMadLib()
-  var storyData = getMadLib();
+  var storyData = getStory();
+  var storyname = document.getElementById("noun6").value;
+  //this method saves the madilib to the database
+  console.log("saveMadLib()" + storyData);
   //them, save the storyName and storyData object to the database
-  db.collection("madlibs").doc(storyData.storyName).set(storyData);
+  db.collection("madlibsjuan").doc(storyData.storyName).set(storyData);
   alert(storyData.storyName + " saved to database!");
 }
+
 function retrieveMadLib() {
-  //this method retrieves an existing madilib from the database
-  console.log("retrieveMadlib() called");
-  //first, ask the uset the type the storyname they want to retrieve
-  var storyName = prompt("Enter story name to retrieve:"); db.collection("madlibs").doc(storyName).get().then((doc) => {
-    // if the story exists in the database, show it on the website. Else , say "Story not found"
-    if (doc.exists) {
-      console.log("Document data:", doc.data());
-      var storyData = doc.data();
-      document.getElementById("noun6").innerHTML = storyData.story;
-    } else {
-      //doc.data()will be undefined in this case
-      console.log("No such document!");
-      document.getElementById("noun6").innerHTML = "Story not found";
-    }
-  })
+  //first, ask the used the type the storyname they want to retrieve
+  var storyData = prompt("Enter story name to retrieve:"); db.collection("madlibsjuan")
+    .doc(storyData)
+    .get()
+    .then((doc) => {
+      // if the story exists in the database, show it on the website. Else , say "Story not found"
+      if (doc.exists) {
+        console.log("Document data:", doc.data());
+        var storyData = doc.data();
+        console.log("storyName: " + storyData.storyName);
+        document.getElementById("noun1").innerHTML = storyData.noun1;
+        document.getElementById("noun2").innerHTML = storyData.noun2;
+        document.getElementById("adjective").innerHTML = storyData.adjective;
+        document.getElementById("noun3").innerHTML = storyData.noun3;
+        document.getElementById("verb").innerHTML = storyData.verb;
+        document.getElementById("noun4").innerHTML = storyData.noun4;
+        document.getElementById("noun5").innerHTML = storyData.noun5;
+        document.getElementById("noun6").innerHTML = storyData.storyName;
+      } else {
+        //doc.data()will be undefined in this case
+        console.log("No such document!");
+        document.getElementById("noun6").innerHTML = "Story not found";
+      }
+      //this method retrieves an existing madilib from the database
+      console.log("retrieveMadlib()" + storyName);
+    })
     .catch((error) => {
       console.log("Error getting document:", error);
       document.getElementById("noun6").innerHTML = "Story not found";
@@ -80,10 +97,10 @@ function retrieveMadLib() {
 }
 function editMadLib() {
   //this methos allow the user to edit an existing madilib in the database
-  console.log("editMadlib() called")
+  console.log("editMadlib()" + storyData);
   //first , ask the user to retrieve an existing madlib from database
-  var storyName = prompt("Enter story name to edit:"); db.collection("madlibs")
-    .doc(storyName)
+  var storyData = prompt("Enter story name to edit:"); db.collection("madlibsjuan")
+    .doc(storyData)
     .get()
     .then((doc) => {
       if (doc.exists) {
@@ -108,15 +125,14 @@ function editMadLib() {
       console.log("Error getting document:", error);
       document.getElementById("noun6").value = "Story not found";
     })
-
 }
+
 function deleteMadLib() {
   // this method deletes an existing madilib from the database and delete it.
   console.log("deleteMadlib() called");
   //first  ask the user to type the story name they want to delete.\
-  var storyName = prompt("Enter story name to delete:");
-  db.collection("madlibs")
-    .doc(storyName)
+  var storyData = prompt("Enter story name to delete:");db.collection("madlibsjuan")
+    .doc(storyData)
     .get()
     .then((doc) => {
       // if the story exists in the database, delete it. Else, display "Story not found"
